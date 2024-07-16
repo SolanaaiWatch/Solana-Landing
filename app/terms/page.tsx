465ca5c20c15fc5f1ea9d277a91ad0f1fd8b751f
Devnuel <emmanuelifegwu@gmail.com>
2024-07-16 15:52:19 +0100

    Terms and privacy page

---
+++ app/terms/page.tsx
@@ -0,0 +1,92 @@
+import './terms.css'
+
+export default function Terms() {
+
+    return(
+        <div>
+            <div className="terms-container">
+                <h1 className="terms-header">Terms Of Use</h1>
+                <div className="introduction">
+                    <h2 className="into-header">Introduction</h2>
+                    <p className="into-content">Welcome to Solana AI Watch! These Terms of Use ("Terms") govern your access to and use of our website 
+                        solanaaiwatch.com ("Site") and our products, including the Solana AI Watch ("Product").
+                         By using our Site and purchasing our Product, you agree to comply with and be bound by these Terms. 
+                      If you do not agree to these Terms, please do not use our Site or purchase our Product.</p>
+                </div>
+                <div className="changes-to-terms">
+                    <h2 className="changes-to-terms-header">Changes to Terms</h2>
+                    <p className="changes-to-terms-content">We reserve the right to modify these Terms at any time. Any changes will be effective immediately upon posting on the Site.
+                         Your continued use of the Site after any changes constitute your acceptance of the new Terms.</p>
+                </div>
+                <div className="use-of-site">
+                    <h2 className="use-of-site-header">Use of the Site and Product Eligibilty</h2>
+                    <p className="use-of-site-content">You must be at least 18 years old to use this Site and purchase our Product. 
+                        By using the Site, you represent and warrant that you are at least 18 years old.
+                    </p>
+                </div>
+                <div className="user-conduct">
+                    <h2 className="user-conduct-header">User Conduct</h2>
+                    <p className="user-conduct-content">You agree to use the Site and Product only for lawful purposes and in accordance with these Terms. You agree not to use the Site and Product:<br/><br/>
+                    In any way that violates any applicable federal, state, local, or international law or regulation.<br/>
+                    For the purpose of exploiting, harming, or attempting to exploit or harm minors in any way.<br/>
+                    To send, knowingly receive, upload, download, use, or re-use any material that does not comply with these Terms.<br/>
+                    To transmit, or procure the sending of, any advertising or promotional material, including any "junk mail," "chain letter," "spam," or any other similar solicitation.<br/>
+                    To impersonate or attempt to impersonate Solana AI Watch, a Solana AI Watch employee, another user, or any other person or entity.<br/>
+                    To engage in any other conduct that restricts or inhibits anyone's use or enjoyment of the Site or Product, or which, as determined by us, may harm Solana AI Watch or users of the Site, or expose them to liability.
+                    Intellectual Property<br/>
+                    The Site and its entire contents, features, and functionality (including but not limited to all information, software, text, displays, images, video, and audio, and the design, selection, and arrangement thereof) 
+                    are owned by Solana AI Watch, its licensors, or other providers of such material and are protected by United States and international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.
+                    </p>
+                </div>
+                <div className="product-use">
+                    <h2 className="product-use-header">Product Use</h2>
+                    <p className="product-use-content">Your use of the Solana AI Watch is subject to all applicable laws and regulations and you are solely responsible for ensuring compliance. The Product is provided "as is" without any warranties, express or implied, including but not 
+                        limited to the implied warranties of merchantability and fitness for a particular purpose.
+                    </p>
+                </div>
+                <div className="limitation">
+                    <h2 className="limitation-header">Limitation of Liability </h2>
+                    <p className="limitation-content">To the fullest extent provided by law, Solana AI Watch shall not be liable for any damages arising out of or related to your use of the Services, including but not limited to direct, indirect, incidental, consequential, or punitive damages.
+                    </p>
+                </div>
+                <div className="termination">
+                    <h2 className="termination-header">Termination</h2>
+                    <p className="termination-content">We may terminate your access to the Services at any time, for any reason, without notice.
+                    </p>
+                </div>
+                <div className="indemnification">
+                    <h2 className="indemnification-header">Indemnification</h2>
+                    <p className="indemnification-content">You agree to indemnify and hold harmless Solana AI Watch, its officers, directors, employees, agents, and licensors from and against any and all claims, demands, losses, liabilities, costs, or expenses (including attorneys' fees) arising out of or related to your use of the Services.
+                    </p>
+                </div>
+                <div className="govern">
+                    <h2 className="govern-header">Governing Laws</h2>
+                    <p className="govern-content">These Terms shall be governed by and construed in accordance with the laws of the United States, without regard to its conflict of law provisions.
+                    </p>
+                </div>
+                {/* <div className="dispute">
+                    <h2 className="dispute-header">Dispute Resolution</h2>
+                    <p className="dispute-content">Any dispute arising out of or related to these Terms shall be resolved by [arbitration/mediation] in accordance with the rules of [arbitration/mediation institution]
+                    </p>
+                </div> */}
+                <div className="entire">
+                    <h2 className="entire-header">Entire Agreement</h2>
+                    <p className="entire-content">These Terms constitute the entire agreement between you and Solana AI Watch regarding your use of the Services.
+                    </p>
+                </div>
+                <div className="sev">
+                    <h2 className="sev-header">Severability</h2>
+                    <p className="sev-content">If any provision of these Terms is held to be invalid or unenforceable, such provision shall be struck and the remaining provisions shall remain in full force and effect.
+                    </p>
+                </div> 
+                <div className="contact">
+                    <h2 className="contact-header">Contact Us</h2>
+                    <p className="contact-content">If you have any questions regarding these Terms, please contact us at support@solanaaiwatch.com.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    )
+    
+}
